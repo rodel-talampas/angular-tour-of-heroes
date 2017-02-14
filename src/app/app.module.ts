9006a0ef0f8264ce6f441ae10bbdf57d3894e7c6
rodel-talampas <rodel.talampas@helixleisure.com>
2017-02-14 08:32:40 +0800

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
+import { HeroSearchComponent }         from './hero-search.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule ,AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService) ],
+  declarations: [ AppComponent, HeroDetailComponent ,HeroesComponent, DashboardComponent, HeroSearchComponent],
+  bootstrap:    [ AppComponent ],
+  providers: [
+    HeroService
+  ]
 })
 export class AppModule { }
